63e40fdd3e02036442b6c0cd2e33876f982ed362
cantutar <vahitcant@gmail.com>
2022-10-03 04:19:06 +0300

    finished hero

---
+++ src/main.js
@@ -0,0 +1,21 @@
+// "use strict";
+// exports.__esModule = true;
+// require("./style.css");
+// var Openbtn = document.getElementById("Open");
+// var Closebtn = document.getElementById("Close");
+// var Navbar = document.querySelector("#Navbar");
+// Openbtn.addEventListener("click", function () {
+//   Navbar.classList.remove("hidden");
+// });
+// Closebtn.addEventListener("click", function () {
+//   Navbar.classList.add("hidden");
+// });
+const Openbtn = document.getElementById("Open");
+const Closebtn = document.getElementById("Close");
+const Navbar = document.querySelector("#Navbar");
+Openbtn.addEventListener("click", () => {
+  Navbar.classList.remove("hidden");
+});
+Closebtn.addEventListener("click", () => {
+  Navbar.classList.add("hidden");
+});
